9324c1e14724203fa10360e9a33d379197e8d58c
Leandro Ordonez <leandro.ordonezante@ugent.be>
2017-02-13 18:07:23 +0100

    fix endless loop due to state mishandling

--- reactive/superset.py
+++ reactive/superset.py
@@ -1,6 +1,7 @@
 import subprocess
 import pexpect
 import os
+import sys
 from charms.reactive import when, when_not, set_state
 from charmhelpers.core import hookenv
 from charmhelpers.core.hookenv import status_set
@@ -23,21 +24,24 @@ def setup():
       'apt.installed.libldap2-dev')
 def install_superset():
     status_set('maintenance', 'Installing superset')
-#    subprocess.check_call(['pip','install', 'virtualenv'])
-#    subprocess.check_call(['virtualenv', 'env'])
-#    subprocess.check_call(['.', './venv/bin/activate'])
+    #init_virtualenv()
     hookenv.log('Installing Superset')
     subprocess.check_call(['pip','install', '--upgrade', 'setuptools', 'pip'])
     subprocess.check_call(['pip','install', 'superset'])
     set_state('superset.installed')
 
+    hookenv.log('Switching to user ubuntu')
+    subprocess.check_call(['su', '-', 'ubuntu'])
+    hookenv.log('whoami: %s' % subprocess.check_output(['whoami']).strip())
+    hookenv.log('cwd: %s' % os.getcwd())
+
 @when('superset.installed')
+@when_not('superset.configured')
 def superset_setup():
     status_set('maintenance', 'Configuring superset')
     # Setting LC_ALL and LANG vbles
     os.environ['LC_ALL'] = 'C.UTF-8'
     os.environ['LANG'] = 'C.UTF-8'
-
     # Create an admin user (you will be prompted to set username, first and last name before setting a password)
     # Username [admin]:
     # User first name [admin]:
@@ -48,17 +52,17 @@ def superset_setup():
     hookenv.log('Creating admin user for Superset')
     child = pexpect.spawn('fabmanager create-admin --app superset')
     child.expect('\\r\\nUsername \[admin\]: ')
-    child.send('admin')
+    child.sendline('admin')
     child.expect('\\r\\nUser first name \[admin\]: ')
-    child.send('admin')
+    child.sendline('admin')
     child.expect('\\r\\nUser last name \[user\]: ')
-    child.send('user')
+    child.sendline('user')
     child.expect('\\r\\nEmail \[admin@fab.org\]: ')
-    child.send('admin@fab.org')
+    child.sendline('admin@fab.org')
     child.expect('\\r\\nPassword: ')
-    child.send('admin')
+    child.sendline('admin')
     child.expect('\\r\\nRepeat for confirmation: ')
-    child.send('admin')
+    child.sendline('admin')
     # Initialize the database
     hookenv.log('Initialize the database')
     subprocess.check_call(['superset','db', 'upgrade'])
@@ -67,13 +71,44 @@ def superset_setup():
     subprocess.check_call(['superset', 'load_examples'])
     set_state('superset.configured')
 
-@when('superset.configured')
+@when('superset.installed',
+      'superset.configured')
+@when_not('superset.ready')
 def superset_startup():
     # Create default roles and permissions
     hookenv.log('Create default roles and permissions')
     subprocess.check_call(['superset', 'init'])
     # Start the web server on port 8088, use -p to bind to another port
-    hookenv.log('Start the web server on port 8088')
-    subprocess.check_call(['superset', 'runserver'])
-    set_state('superset.running')
-    status_set('ready', 'Superset up and running')
+    #hookenv.log('Start the web server on port 8088')
+    #subprocess.check_call(['superset', 'runserver'])
+    set_state('superset.ready')
+    status_set('active', 'Superset up and running')
+
+def init_virtualenv():
+    venv = os.path.abspath('./venv')
+    hookenv.log('Virtualenv path: %s' % venv)
+    vbin = os.path.join(venv, 'bin')
+    vpip = os.path.join(vbin, 'pip')
+    vpy = os.path.join(vbin, 'python')
+
+    hookenv.log('Installing virtualenv')
+    subprocess.check_call(['pip','install', 'virtualenv'])
+
+    hookenv.log('Switching to user ubuntu')
+    subprocess.check_call(['su', '-', 'ubuntu'])
+
+    hookenv.log('Creating a virtualenv in %s' % venv)
+    subprocess.check_call(['virtualenv', '--python=python3', 'venv'])
+
+    hookenv.log('Activating venv')
+    os.environ['PATH'] = ':'.join([vbin, os.environ['PATH']])
+    reload_interpreter(vpy)
+
+
+def reload_interpreter(python):
+    """
+    Reload the python interpreter to ensure that all deps are available.
+    Newly installed modules in namespace packages sometimes seemt to
+    not be picked up by Python 3.
+    """
+    os.execle(python, python, sys.argv[0], os.environ)
